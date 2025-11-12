3a2da31d3093b5bfb0cafb7126e1333653766f73
Skander Lopez <skanderlopez19@gmail.com>
2025-11-11 21:32:20 -0300

    feat: Implementar modularizacion del footer

--- GeoWeb/components.js
+++ GeoWeb/components.js
@@ -23,3 +23,27 @@ class Header extends HTMLElement {
 }
 
 customElements.define('main-header', Header);
+
+// CÓDIGO DEL FOOTER (Añadir debajo del código del Header)
+
+class Footer extends HTMLElement {
+    constructor() {
+        super();
+    }
+
+    connectedCallback() {
+        this.innerHTML = `
+            <footer>
+                <div class="footer-content">
+                    <p>&copy; 2025 GeoFinance Intel. Análisis Automatizado.</p>
+                    <div class="social-links">
+                        <a href="https://linkedin.com/in/skanderlopez19" target="_blank">LinkedIn</a> |
+                        <a href="mailto:skanderlopez19@gmail.com">Contacto</a>
+                    </div>
+                </div>
+            </footer>
+        `;
+    }
+}
+
+customElements.define('main-footer', Footer);
